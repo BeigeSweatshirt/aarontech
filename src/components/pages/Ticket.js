012db31f21c048fec16bd2c3b7f1241d06146af6
matt <mholford@protonmail.com>
2021-11-28 16:08:34 -0800

    minor changes

--- src/components/pages/Ticket.js
+++ src/components/pages/Ticket.js
@@ -9,45 +9,45 @@ function Ticket(props) {
   // sessionStorage.setItem("USER", USER);
   // console.log(sessionStorage.getItem("NAME"));
   // history("/account");
-}
+  const history = useNavigate();
 
-// const handleFormSubmit = (event) => {
-const navigate = useNavigate();
-//   history("/account");
-//   props.history.push("/account",
-//     { state: document.getElementById('desc').value }
-//   );
-// }
+  function handleSumbmit() {
+    const USER = document.getElementById('user').value;
+    // alert(name);
+    sessionStorage.setItem("USER", USER);
+    // console.log(sessionStorage.getItem("NAME"));
+    history("/account");
+  }
 
-return (
-  <div className="App" >
-    <form onSubmit={handleSumbmit}>
-      <input
-        type="text"
-        id="uname"
-        placeholder="Username">
-      </input>
-      <input
-        type="text"
-        id="email"
-        placeholder="Email">
-      </input>
-      <input
-        type="text"
-        id="pword"
-        placeholder="Password">
-      </input>
-      <input
-        type="text"
-        id="desc"
-        placeholder="Describe your problem">
-      </input>
-      <input
-        type="submit"
-        value="Submit">
-      </input>
-    </form>
-  </div>
-);
+  return (
+    <div className="App" >
+      <form onSubmit={handleSumbmit}>
+        <input
+          type="text"
+          id="uname"
+          placeholder="Username">
+        </input>
+        <input
+          type="text"
+          id="email"
+          placeholder="Email">
+        </input>
+        <input
+          type="text"
+          id="pword"
+          placeholder="Password">
+        </input>
+        <input
+          type="text"
+          id="desc"
+          placeholder="Describe your problem">
+        </input>
+        <input
+          type="submit"
+          value="Submit">
+        </input>
+      </form>
+    </div>
+  );
 }
 export default Ticket;

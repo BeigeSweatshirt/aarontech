2b7933bbcf2ea55e809a186a53d7b1aa57821e84
Jacob <jacobfisher311@gmail.com>
2021-11-29 16:07:32 -0800

    final commitment completing css folder v4

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
-import './HeroSection.css'
-import '../css/App.css';
+import './css/HeroSection.css'
+import './css/App.css';
 import Grid from './Grid';
 import GetStartedBtn from './GetStartedBtn';
 

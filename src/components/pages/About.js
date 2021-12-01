40d309149064c1429726dcb065a1ad2a714979a4
Jacob <jacobfisher311@gmail.com>
2021-11-30 16:33:58 -0800

    synced aboutcss to aboutjs, and slightly adjusted the about us page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../css/Cards.css';
+import '../css/About.css'
 import CardItems from '../widgets/CardItems'
 import shiva from '../../assets/Shiva_selfie.JPG'
 import jacob from '../../assets/jacob.jpg'
@@ -9,6 +10,7 @@ function Cards() {
   return (
     <div className='cards'>
       <h1>Our Team</h1>
+      <h2 >AaronTech is a technology repair company that specializes in Windows computer repairs. </h2>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
